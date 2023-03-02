ed54d546c6bfe8a0ba9652f4e1c269d7b76c6703
roganjoshu <56878631+roganjoshu@users.noreply.github.com>
2023-03-02 18:25:53 +0000

    Initial commit

---
+++ Organise-Photos.py
@@ -0,0 +1,55 @@
+from PIL import Image
+import os
+from datetime import datetime
+
+def ProcessImages():    
+    directoryAsString = input("Enter path to folder: ")
+    AssessPhotos(directoryAsString)
+    
+def AssessPhotos(directoryAsString):
+    for fileName in os.listdir(directoryAsString):
+        
+        if fileName.endswith(".jpg") or fileName.endswith(".mp4"):
+            
+            #old file path is location appended with name
+            oldFilePath = os.path.join(directoryAsString, fileName)
+            #Identify new location for file we're assessing
+            newFileLocation = os.path.join(directoryAsString, GetCreationDate(oldFilePath))
+            
+            #If the new location doesn't exist then create the new location
+            if not os.path.exists(newFileLocation):
+                os.mkdir(newFileLocation)
+            #Move the image to the new location
+            MoveFileToFolder(oldFilePath, newFileLocation, fileName)
+                
+def GetCreationDate(p_filePath):
+    
+    #Retrieve .jpg creation date
+    if p_filePath.endswith(".jpg"):
+        GetImageCreationDate(p_filePath)
+    
+    #Retrieve .mp4 creation date
+    elif p_filePath.endswith(".mp4"):
+        GetFilmCreationDate(p_filePath)
+        
+    
+def GetImageCreationDate(p_filePath):
+        exifData = Image.open(p_filePath)._getexif()[36867]
+        exifCreationDate = exifData.split(":")[0] + "-" + exifData.split(":")[1]
+        return exifCreationDate
+    
+def GetFilmCreationDate(p_filePath):
+    creationDate = os.path.getmtime(p_filePath)
+    modified_datetime = datetime.fromtimestamp(creationDate)
+    modified_datetime_str = modified_datetime.strftime("%Y-%m-%d")
+    return modified_datetime_str.split("-")[0] + "-" + modified_datetime_str.split("-")[1]
+        
+def MoveFileToFolder(p_oldFilePath, p_newFileLocation, p_fileName):
+    newFilePath = os.path.join(p_newFileLocation, p_fileName)
+    if os.path.exists(newFilePath):
+        print("File exists in location")
+    else:
+        os.rename(p_oldFilePath, newFilePath)
+
+if __name__ == "__main__":
+    ProcessImages()
